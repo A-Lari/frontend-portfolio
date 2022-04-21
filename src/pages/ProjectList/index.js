073017662b6c6480d476b811b3e68038c5f91f7d
A-Lari <lari.anthony84@gmail.com>
2022-04-21 13:46:34 +0200

    ajout route GET/projects

--- src/pages/ProjectList/index.js
+++ src/pages/ProjectList/index.js
@@ -1,10 +1,30 @@
-import { Button } from 'react-bootstrap';
+import { Button } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import services from "../../services";
+
 export default function ProjectList() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    services
+      .getProjects()
+      .then((response) => {
+        console.log(response);
+        setProjects(response);
+      })
+      .catch(console.log);
+  }, []);
   return (
     <>
-    <h2>Liste Project</h2>
-    <Button variant="primary">TEST1</Button>{' '}
-    <Button variant="secondary">TEST2</Button>{' '}
+      <div>
+        <h1>Liste projets</h1>
+        {projects.map((project) => (
+          <p key={project._id}>
+            <span>{project.title}</span>
+            <span>{project.summary}</span>
+          </p>
+        ))}
+      </div>
     </>
-  )
+  );
 }
--- src/services.js
+++ src/services.js
@@ -8,6 +8,10 @@ const services = {
   getInfos() {
     return base.get(`/sites/6261254d4c2647fc4c5f6b35`).then((res) => res.data);
   },
+
+  getProjects() {
+    return base.get(`/projects`).then((res) => res.data);
+  },
 };
 
 export default services;
