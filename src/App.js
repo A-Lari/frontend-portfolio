3d84e411a59b8d5bc3605003171bd858c458f637
Christian ly <christian_ly@outlook.fr>
2022-04-25 12:34:40 +0200

    Ajout routes projets + Login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-
 import Home from "./pages/Home";
-import ProjectList from "./pages/ProjectList";
-import NavBar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ProjectList from "./pages/ProjectList";
+import ProjectDetail from "./pages/ProjectDetail";
+import CreateProject from "./pages/CreateProject";
+
+import SignupPage from "./pages/Auth/Signup";
+import LoginPage from "./pages/Auth/Login";
+import NavBar from "./components/Navbar";
 
 function App() {
+  const [connected, setConnected] = useState(false);
+
+  useEffect(() => {
+    const hasJwt = localStorage.getItem("jwt"); // EzbBlablablalba
+    setConnected(Boolean(hasJwt)); // on convertit en booléen
+  }, []);
+
   return (
     <div className="App">
-      <NavBar />
-
+      <NavBar connected={connected} setConnected={setConnected}/>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home connected={connected} />} />
         <Route path="/project" element={<ProjectList />} />
+        <Route path="/project/create" element={<CreateProject connected={connected} />} />
+        <Route path="/project/:idProject" element={<ProjectDetail />} />
+        <Route
+        path="/login"
+        element={
+          <LoginPage connected={connected} setConnected={setConnected} />
+        }
+        />
+        <Route
+        path="/signup"
+        element={<SignupPage connected={connected} />}
+        />
       </Routes>
-
       <Footer />
     </div>
   );
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,20 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "./Logo";
 import "./Navbar.css";
 
-export default function NavBar() {
+export default function NavBar({ connected, setConnected }) {
+  const navigate = useNavigate();
+
+  function disconnect() {
+    navigate("/");
+    setConnected(false);
+    localStorage.removeItem("jwt");
+  }
+
   return (
     <div className="navbar-perso">
       <Logo />
 
       <div className="nav-links">
-        <Link className="home-link" to="/">
-          Home
-        </Link>
-        <Link className="project-link" to="/project">
-          Projects
-        </Link>
+        <Link className="home-link" to="/">Home</Link>
+        <Link className="project-link" to="/project">Projects</Link>
+        {connected === true && <Link className="project-link" to="/project/create">Create project</Link>}
+        {connected === true && <a className="project-link" href="#" onClick={disconnect}>Se déconnecter</a>}
+        {connected === false && <Link className="project-link" to="/login">Se connecter</Link>}
+        {connected === false && <Link className="project-link" to="/signup">S'inscrire</Link>}  
       </div>
     </div>
   );
